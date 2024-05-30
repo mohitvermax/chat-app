577e4b4c1944218c20918559ef67311f5ed72ca3
Mohit Verma <mohit_v@ch.iitr.ac.in>
2024-05-30 17:49:00 +0530

    Setup DB and User model

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,8 +1,58 @@
-export const signUp = (req,res) => {
+import bcrypt from 'bcryptjs';
+import User from "../models/user.model.js";
+
+export const signUp = async (req,res) => {
     try {
-        const {fullName, username , password, confirmpassword} = req.body;
-    } catch (error) {
+        const {fullName, username , password, confirmPassword, gender} = req.body;
+
+        if(password !== confirmPassword) {
+            return res.status(400).json({
+                error: "Passwords do not match"
+            })
+        }
+
+        const user = await User.findOne({username}); //this will check if user with username exists in db or not
+
+        if(user) {
+            return res.status(400).json({
+                error: "User already exists"
+            })
+        }
+
+        //Hash the password here 
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt); 
+        //https://avatar-placeholder.iran.liara.run/ => for avatars
         
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+        const newUser = new User({
+            fullName,
+            username,
+            password: hashedPassword,
+            gender, 
+            profilePic: gender === 'male' ? boyProfilePic : girlProfilePic
+        })
+
+        if(newUser){
+            //Generate JWT Token here 
+            await newUser.save();
+
+        res.status(201).json({
+            _id : newUser._id,
+            fullName: newUser.fullName,
+            username: newUser.username,
+            gender: newUser.gender,
+            profilePic: newUser.profilePic
+        })
+        }else{
+            res.status(500).json({error:"Invalid user data"});
+        }
+
+    } catch (error) {
+        console.log("Error in signup controller", error.message);
+        res.status(500).json({error:"Internal server error"}); 
     }
 }
 
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,24 @@
 import express from 'express';
 import dotenv from 'dotenv'
 import authRoutes from './routes/auth.routes.js';
-
-const app = express()
-
+import connectToDB from './db/connectToDB.js';
 dotenv.config()
+const app = express()
 const PORT = process.env.PORT || 3000
+
+
+
 
 app.get('/',(req,res) => {
     res.send("Hello World!!")
 })
 
+
+// Middlewares
+app.use(express.json()) // to extract the fields from req.body
 app.use("/api/auth", authRoutes)
 
-app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
+app.listen(PORT, () => {
+    connectToDB();
+    console.log(`server is running on port ${PORT}`)
+})
