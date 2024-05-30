dce769019a265626b70b29c48cca5669bf98fd59
Mohit Verma <mohit_v@ch.iitr.ac.in>
2024-05-30 18:02:24 +0530

    JWT added

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import bcrypt from 'bcryptjs';
 import User from "../models/user.model.js";
+import generateTokenAndSetCookie from '../utils/generateToken.js';
 
 export const signUp = async (req,res) => {
     try {
@@ -37,6 +38,8 @@ export const signUp = async (req,res) => {
 
         if(newUser){
             //Generate JWT Token here 
+            generateTokenAndSetCookie(newUser._id , res);
+
             await newUser.save();
 
         res.status(201).json({
