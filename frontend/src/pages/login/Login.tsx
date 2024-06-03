ba37755d327cb33f877ed5f59e88f9f38da48dec
Mohit Verma <mohit_v@ch.iitr.ac.in>
2024-06-03 13:06:13 +0530

    Frontend designs added

---
+++ frontend/src/pages/login/Login.tsx
@@ -0,0 +1,40 @@
+const Login = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+        <div className="rounded-lg w-full p-6 shadow-md bg-gray-800 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+            <h1 className="text-3xl font-semibold text-center text-gray-300">
+                Login
+                <span className="text-blue-500"> ChatApp</span>
+            </h1>
+
+
+            <form className="mt-6">
+                <div className="">
+                    <label className="label p-2">
+                        <span className="text-base label-text">Username</span>
+                    </label>
+                    <input type="text" placeholder="Enter your username" className="w-full input input-bordered h-10" />
+                </div>
+
+                <div className="">
+                    <label className="label p-2">
+                        <span className="text-base label-text">Password</span>
+                    </label>
+                    <input type="password" placeholder="Enter your passsword" className="w-full input input-bordered h-10" />
+                </div>
+                <a href="">
+                "Don't have an account yet?"
+                </a>
+
+                <div className="mt-6">
+                    <button type="submit" className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg focus:outline-none focus:shadow-outline">
+                        Login
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default Login
